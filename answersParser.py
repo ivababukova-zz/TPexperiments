afd6283a5a3a3a87f5fb1f917e6c7383a60b6d4c
Iva Babukova <iva.babukova@skyscanner.net>
2017-03-10 14:22:15 +0000

    Addded a stacked bar with number of failed to generate, number of sat and number of unsat

--- answersParser.py
+++ answersParser.py
@@ -17,6 +17,23 @@ def separateFiles(filenames):
             ipfiles.append(name)
     return(cpfiles, ipfiles)
 
+def separateOptFuncs(filenames):
+    cost = []
+    duration = []
+    conn = []
+    flights = []
+    for name in filenames:
+        optfunc = name.split("_")[-2]
+        if optfunc == "cost":
+            cost.append(name)
+        elif optfunc == "connections":
+            conn.append(name)
+        elif optfunc == "trip":
+            duration.append(name)
+        elif optfunc == "flights":
+            flights.append(name)
+    return cost,duration,conn,flights
+
 # given a filename, returns its running time
 def runningTime(filename):
     with open(filename, "r") as f:
@@ -28,6 +45,11 @@ def runningTime(filename):
 
 def getInstanceProps(filename):
     instance = filename.split("/")[-1].split("_")[0:4]
+    instance = '_'.join(instance)
+    return(instance)
+
+def getInstanceName(filename):
+    instance = filename.split("/")[-1].split("_")[0:5]
     instance = '_'.join(instance)
     return(instance)
 
@@ -75,72 +97,107 @@ def parseForParallelPlot(cpdata, ipdata, instanceNames):
             ipline = ",".join([str(ip), m, n, d, T, "ip"])
             f.write(cpline + "\n" + ipline + "\n")
 
-def groupByM(cpruntimes, ipruntimes):
-    sortedcp = sorted(cpruntimes, key=itemgetter(0,1,2,3))
-    sortedip = sorted(ipruntimes, key=itemgetter(0,1,2,3))
+# returns running times, instances, grouped by m, n, d or T
+def group(data, pos):
+    sorteddata = sorted(data, key=itemgetter(pos))
+    vals = [val[pos] for val in sorteddata]
+    instanceNames = [val[4] for val in sorteddata]
+    newdata = [val[5] for val in sorteddata]
+    if pos == 0:
+        xlabel = "Number of Flights"
+    elif pos == 1:
+        xlabel = "Number of Airports"
+    elif pos == 2:
+        xlabel = "Number of Destinations"
+    elif pos == 3:
+        xlabel = "Holiday Time"
+    else:
+        print("The pos index specified is wrong!!!!")
+    return newdata, instanceNames, vals, xlabel
 
-    mvals = [val[0] for val in sortedcp]
-    instanceNames = [val[4] for val in sortedip]
+def satVsUnsat(ipfiles):
+    unsatFiles = []
+    satFiles = []
+    for ip in ipfiles:
+        sat = False
+        with open(ip, "r") as f:
+            for line in f.readlines():
+                line = line.strip().split()
+                props = getInstanceName(ip)
 
-    if len(mvals) != len(instanceNames):
-        print("CP and IP number of results are not equal")
-    cpdata = [val[5] for val in sortedcp]
-    ipdata = [val[5] for val in sortedip]
-    xlabel = "Number of Flights"
-    return cpdata, ipdata, mvals, xlabel
+                if len(line) > 0 and line[0] == "Schedule:":
+                    sat = True
+                    if len(satFiles) == 0 or satFiles[-1] != props:
+                        satFiles.append(props)
+            if not sat and (len(unsatFiles) == 0 or unsatFiles[-1] != props):
+                unsatFiles.append(props)
+    return satFiles, unsatFiles
 
-def groupByN(cpruntimes, ipruntimes):
-    sortedcp = sorted(cpruntimes, key=itemgetter(1,2,0,3))
-    sortedip = sorted(ipruntimes, key=itemgetter(1,2,0,3))
+def countLines(filename):
+    with open(filename, "r") as f:
+        lines = f.readlines()
+        return len(lines)
 
-    nvals = [val[1] for val in sortedcp]
-    instanceNames = [val[4] for val in sortedip]
+def compareCPandIP(filenames):
+    cpfiles, ipfiles = separateFiles(filenames)
+    cpruntimes = runningtimes(cpfiles)
+    ipruntimes = runningtimes(ipfiles)
+    pos = 0
+    cpdata, instanceNames, vals, xlabel = group(cpruntimes, pos)
+    ipdata, instanceNames, vals, xlabel = group(ipruntimes, pos)
+    # plotMaker.makePlots(cpdata, ipdata, vals, xlabel)
+    ylabel = "Running Times (sec)"
+    plotMaker.twoHistograms(cpdata, ipdata, vals, xlabel, ylabel)
+    # plotname = "Running time of CP and IP models"
+    # parseForParallelPlot(cpdata, ipdata, instanceNames)
+    # plotMaker.parallelCoordinates(cpdata, ipdata)
 
-    if len(nvals) != len(instanceNames):
-        print("CP and IP number of results are not equal")
-    cpdata = [val[5] for val in sortedcp]
-    ipdata = [val[5] for val in sortedip]
-    xlabel = "Number of Airports"
-    return cpdata, ipdata, nvals, xlabel
+def compareOptFuncs(filenames):
+    cost,duration,conn,flights = separateOptFuncs(filenames)
 
-def groupByD(cpruntimes, ipruntimes):
-    sortedcp = sorted(cpruntimes, key=itemgetter(2,0,1,3))
-    sortedip = sorted(ipruntimes, key=itemgetter(2,0,1,3))
+    costruntimes = runningtimes(cost)
+    durationruntimes = runningtimes(duration)
+    connruntimes = runningtimes(conn)
+    flightsruntimes = runningtimes(flights)
 
-    dvals = [val[2] for val in sortedcp]
-    instanceNames = [val[4] for val in sortedip]
+    pos = 0
 
-    if len(dvals) != len(instanceNames):
-        print("CP and IP number of results are not equal")
-    cpdata = [val[5] for val in sortedcp]
-    ipdata = [val[5] for val in sortedip]
-    xlabel = "Number of Destinations"
-    return cpdata, ipdata, dvals, xlabel
+    costdata, instanceNames, vals, xlabel = group(costruntimes, pos)
+    durationdata, instanceNames, vals, xlabel = group(durationruntimes, pos)
+    conndata, instanceNames, vals, xlabel = group(connruntimes, pos)
+    flightsdata, instanceNames, vals, xlabel = group(flightsruntimes, pos)
+    print(costdata)
+    print(durationdata)
+    print(conndata)
+    print(flightsdata)
+    ylabel = "Running Times (sec)"
+    plotMaker.histograms(costdata,durationdata,conndata,flightsdata,vals, xlabel, ylabel)
 
-def groupByT(cpruntimes, ipruntimes):
-    sortedcp = sorted(cpruntimes, key=itemgetter(3,0,1,2))
-    sortedip = sorted(ipruntimes, key=itemgetter(3,0,1,2))
+# returns a plot with one bar with number of unsat and one bar with number of sat instances
+def compareSatvsUnsat(filenames):
+    cpfiles, ipfiles = separateFiles(filenames)
+    satFiles, unsatFiles = satVsUnsat(ipfiles)
+    xlabel = ""
+    plotname = "SAT vs UNSAT TP instances"
+    plotMaker.twoBarsOnce(len(satFiles), len(unsatFiles), ["SAT", "UNSAT"], xlabel, "", plotname)
 
-    tvals = [val[3] for val in sortedcp]
-    instanceNames = [val[4] for val in sortedip]
+# aims to generate a cumulative bar with total configs, successful configs, sat and unsat instances
+def dataGeneratorStats(filenames, total):
+    cpfiles, ipfiles = separateFiles(filenames)
+    satFiles, unsatFiles = satVsUnsat(ipfiles)
+    totalSAT = len(satFiles)
+    totalUNSAT = len(unsatFiles)
+    garbageCount = total*5 - (totalSAT+totalUNSAT)
 
-    if len(tvals) != len(instanceNames):
-        print("CP and IP number of results are not equal")
-    cpdata = [val[5] for val in sortedcp]
-    ipdata = [val[5] for val in sortedip]
-    xlabel = "Holidat Time"
-    return cpdata, ipdata, tvals, xlabel
+    data = {
+        'config_name': ['MonteCarlo'],
+        'garbage': [garbageCount],
+        'unsat': [totalUNSAT],
+        'sat': [totalSAT]
+    }
+    print(data)
+    plotMaker.stackedBar(data)
 
+totalCount = 200 # how many configs were given to the data generator
 filenames = sys.argv[1:]
-cpfiles, ipfiles = separateFiles(filenames)
-cpruntimes = runningtimes(cpfiles)
-ipruntimes = runningtimes(ipfiles)
-
-cpdata, ipdata, vals, xlabel = groupByN(cpruntimes, ipruntimes)
-print(cpdata)
-print(ipdata)
-print(vals)
-plotMaker.makePlots(cpdata, ipdata, vals, xlabel)
-
-# parseForParallelPlot(cpdata, ipdata, instanceNames)
-# plotMaker.parallelCoordinates(cpdata, ipdata)
+dataGeneratorStats(filenames, totalCount)
--- plotMaker.py
+++ plotMaker.py
@@ -7,9 +7,9 @@ from pandas.tools.plotting import parallel_coordinates
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import pprint
+import pandas as pd
 
 pp = pprint.PrettyPrinter(indent=4)
-
 
 def getMinMaxY(cpdata, ipdata):
     minYcp = min(cpdata)
@@ -27,8 +27,34 @@ def parallelCoordinates(cpdata, ipdata):
     parallel_coordinates(data, 'Name', color=['red','blue'])
     plt.show()
 
-def twoBars(cpdata, ipdata, width, dvals, xlabel, ylabel, plotname):
+def twoHistograms(a, b, x, xlabel, ylabel):
+    fig, ax = plt.subplots()
+    lw = 1
+    ha = plt.plot(x, a, 'r-', linewidth=lw)
+    hb = plt.plot(x, b, 'b-', linewidth=lw)
+    ax.set_ylabel(ylabel)
+    ax.set_xlabel(xlabel)
+    # ax.set_yscale('log')
+    ax.grid(True)
+    ax.legend((ha[0], hb[0]), ('CP', 'IP'), loc='upper left')
+    plt.show()
 
+def histograms(a, b, c, d, x, xlabel, ylabel):
+    fig, ax = plt.subplots()
+    lw = 1
+    ha = plt.plot(x, a, 'r-', linewidth=lw)
+    hb = plt.plot(x, b, 'b-', linewidth=lw)
+    hc = plt.plot(x, c, 'g-', linewidth=lw)
+    hd = plt.plot(x, d, 'y-', linewidth=lw)
+    ax.set_ylabel(ylabel)
+    ax.set_xlabel(xlabel)
+    # ax.set_yscale('log')
+    ax.grid(True)
+    ax.legend((ha[0], hb[0], hc[0], hd[0]), ('cost', 'duration', 'connections', 'flights'), loc='upper left')
+    plt.show()
+
+def twoBars(cpdata, ipdata, dvals, xlabel, ylabel, plotname):
+    width = 0.5
     N = len(cpdata)
     ind = np.arange(N)  # the x locations for the groups
 
@@ -60,6 +86,111 @@ def twoBars(cpdata, ipdata, width, dvals, xlabel, ylabel, plotname):
     autolabel(rects2)
     plt.show()
 
+# accepts data in this format:
+#data = {
+#     'config_name': ['MonteCarlo'],
+#     'garbage': [garbageCount],
+#     'unsat': [answersScale*totalUNSAT],
+#     'sat': [answersScale*totalSAT]
+# }
+def stackedBar(raw_data):
+    df = pd.DataFrame(raw_data, columns = ['config_name', 'garbage', 'unsat', 'sat'])
+    # Create the general blog and the "subplots" i.e. the bars
+    f, ax1 = plt.subplots(1, figsize=(10,5))
+
+    # Set the bar width
+    bar_width = 0.4
+
+    # positions of the left bar-boundaries
+    bar_l = [i+1 for i in range(len(df['garbage']))]
+
+    # positions of the x-axis ticks (center of the bars as bar labels)
+    tick_pos = [i+(bar_width/2) for i in bar_l]
+
+    # Create a bar plot, in position bar_1
+    ax1.bar(bar_l,
+        # using the pre_score data
+        df['garbage'],
+        # set the width
+        width=bar_width,
+        # with the label pre score
+        label='Failed',
+        # with alpha 0.5
+        alpha=0.5,
+        # with color
+        color='#F4561D')
+
+    # Create a bar plot, in position bar_1
+    ax1.bar(bar_l,
+        # using the mid_score data
+        df['unsat'],
+        # set the width
+        width=bar_width,
+        # with pre_score on the bottom
+        bottom=df['garbage'],
+        # with the label mid score
+        label='UNSAT',
+        # with alpha 0.5
+        alpha=0.5,
+        # with color
+        color='#F1911E')
+
+    # Create a bar plot, in position bar_1
+    ax1.bar(bar_l,
+        # using the post_score data
+        df['sat'],
+        # set the width
+        width=bar_width,
+        # with pre_score and mid_score on the bottom
+        bottom=[i+j for i,j in zip(df['garbage'],df['unsat'])],
+        # with the label post score
+        label='SAT',
+        # with alpha 0.5
+        alpha=0.5,
+        # with color
+        color='#F1BD1A')
+
+    # set the x ticks with names
+    plt.xticks(tick_pos, df['config_name'])
+
+    # Set the label and legends
+    ax1.set_ylabel("Number of TP instances")
+    ax1.grid(True)
+    plt.legend(loc='upper left')
+
+    # Set a buffer around the edge
+    plt.xlim([min(tick_pos)-bar_width, max(tick_pos)+bar_width])
+    plt.show()
+
+def twoBarsOnce(val1, val2, dvals, xlabel, ylabel, plotname):
+    width = 0.5
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(0, val1, width, color='r')
+    rects2 = ax.bar(0 + width, val2, width, color='b')
+
+    # add some text for labels, title and axes ticks
+    ax.set_ylabel(ylabel)
+    ax.set_xlabel(xlabel)
+    ax.set_title(plotname)
+    ylimits = (0,max(val1, val2) + 10)
+    ax.set_xticks([])
+    ax.set_ylim(ylimits) # sets the start and end points on the y axis
+    ax.legend((rects1[0], rects2[0]), ('SAT', 'UNSAT'), loc='lower right')
+
+    def autolabel(rects):
+        """
+        Attach a text label above each bar displaying its height
+        """
+        for rect in rects:
+            height = rect.get_height()
+            ax.text(rect.get_x() + rect.get_width()/2., 1.01*height,
+                    '%.f' % height,
+                    ha='center', va='bottom')
+
+    autolabel(rects1)
+    autolabel(rects2)
+    plt.show()
+
 def separateData(data):
     cpdata = []
     ipdata = []
@@ -73,12 +204,6 @@ def separateData(data):
         if elem[-1] == "]":
             turn = "ip"
     return cpdata, ipdata
-
-def makePlots(cpdata, ipdata, dvals, xlabel):
-    width = 0.5 # the width of the bars
-    ylabel = "Running Time (sec)"
-    plotname = "Running time of CP and IP models"
-    twoBars(cpdata, ipdata, width, dvals, xlabel, ylabel, plotname)
 
 def makeCustomParallelPlot():
     cpdata = []
@@ -120,5 +245,3 @@ def makeCustomParallelPlot():
     # stack the subplots together
     plt.subplots_adjust(wspace=0)
     plt.show()
-
-# makeCustomParallelPlot()
